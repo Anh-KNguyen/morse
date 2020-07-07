e80a919036d0697d056a99d27ace30f8af41c04a
Anh Nguyen <anhknguyen97@yahoo.com>
2020-07-06 22:57:03 -0700

    Create a button to click

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,24 +5,29 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      message: "Mouse Event"
     }
   }
   
   handleEvent = (event) => {
     if(event.type === "mousedown") {
-      this.setState({})
+      this.setState({
+        message: "Mouse Down"
+      });
     }
     else {
-      this.setState({})
+      this.setState({
+        message: "Mouse Up"
+      });
     }
   }
 
   render() {
     return (
       <div>
-
+        <button id="btn" onMouseDown={this.handleEvent} onMouseUp={this.handleEvent}> CLICK </button>
       </div>
+      
     );
   }
 
