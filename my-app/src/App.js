68eaaabf7f916cdf9ed9b3ece0565f5e5688cbdb
Anh Nguyen <anhknguyen97@yahoo.com>
2020-07-17 22:48:48 -0700

    Allign react components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -115,12 +115,23 @@ class App extends React.Component {
   render() {
     return (
       <>
-        
-        
         <div className="row button-container">
           <div className="col-auto">
-            <div className={(this.state.islightOn ? "light-on" : "light-off")} style={{marginLeft: "auto", marginRight: "auto"}}/>
+            <div className={(this.state.islightOn ? "light-on" : "light-off")} style={{marginLeft: "auto", marginRight: "auto", marginTop: "100px", marginBottom: " 100px"}}/>
             <button className={"dot-button " + (this.state.isButtonPushed ? "dot-button-grey" : null)} onMouseDown={this.handleMorseEvent} onMouseUp={this.handleMorseEvent}></button>
+            <br/>
+            <div style={{marginLeft: "auto", marginRight: "auto", textAlign: "center"}}>
+              {this.state.sequence}
+            </div>
+            <br/>
+            <div style={{textAlign: "center"}}>
+              {this.state.letter}
+            </div>
+            <br/>
+            <div style={{textAlign: "center"}}>
+              {this.state.word}
+            </div>
+            <br/>
           </div>
           <div className="col-auto">
             <div>
@@ -134,12 +145,11 @@ class App extends React.Component {
 
         <div>
           {/* {this.state.end_time - this.state.start_time} */}
+          
           <br/>
-          {this.state.sequence}
+          
           <br/>
-          {this.state.letter}
-          <br/>
-          {this.state.word}
+          
           <br/>
           <div class="container">
             <div className="row">
